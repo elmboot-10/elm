16e95a5a0e86989b4cf73637193ca313de38a19c
Nyz99 <2424986966@qq.com>
2020-08-11 19:32:59 +0800

    注册功能

--- src/main/java/com/neusoft/elmboot/controller/FigureController.java
+++ src/main/java/com/neusoft/elmboot/controller/FigureController.java
@@ -3,6 +3,7 @@ package com.neusoft.elmboot.controller;
 import com.neusoft.elmboot.po.Figure;
 import com.neusoft.elmboot.po.User;
 import com.neusoft.elmboot.service.FigureService;
+import com.neusoft.elmboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,5 +19,14 @@ public class FigureController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Figure getFigureByPassword(Figure figure){
         return figureService.getFigureByPassword(figure);
+    }
+    @RequestMapping(value = "/isexistuser")
+    public Integer isExistFigure(Figure figure){
+//        System.out.println("===="+figure);
+        return figureService.isExistFigure(figure);
+    }
+    @RequestMapping(value = "/insert")
+    public int insert(Figure figure){
+        return figureService.insert(figure);
     }
 }
