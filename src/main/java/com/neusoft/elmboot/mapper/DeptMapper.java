59ee16256a6ad14d867ba17a2f1dd7d21b35f54f
yez2252 <719724248@qq.com>
2020-08-13 12:55:58 +0800

    2

--- src/main/java/com/neusoft/elmboot/mapper/DeptMapper.java
+++ src/main/java/com/neusoft/elmboot/mapper/DeptMapper.java
@@ -1,9 +1,12 @@
 package com.neusoft.elmboot.mapper;
 import com.neusoft.elmboot.po.Department;
+import com.neusoft.elmboot.po.Figure;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 @Mapper
 public interface DeptMapper {
-     List<Department> selectDeptList();
+    List<Department> selectDeptList();
+    Integer isExistDept(Department dept);
+    int insert(Department dept);
 }
