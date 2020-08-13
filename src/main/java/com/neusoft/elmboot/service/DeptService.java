0fa2878cae0e5237ae1b092c8813a4b0c7f05ead
Nyz99 <2424986966@qq.com>
2020-08-13 19:25:41 +0800

    部门添加的完善，部门的删除

--- src/main/java/com/neusoft/elmboot/service/DeptService.java
+++ src/main/java/com/neusoft/elmboot/service/DeptService.java
@@ -1,6 +1,7 @@
 package com.neusoft.elmboot.service;
 import com.neusoft.elmboot.po.Department;
 import com.neusoft.elmboot.po.Figure;
+import com.neusoft.elmboot.po.Position;
 
 import java.util.List;
 
@@ -8,5 +9,7 @@ public interface DeptService {
     List<Department> selectDeptList();
     Integer isExistDept(Department dept);
     int insert(Department dept);
+    int update(Department dept);
+    int delete(Department dept);
 }
 
